fa703d5bd6253d2d187f53196f1469cbe0ded0ef
Anthony Biard <39205962+casquetttte@users.noreply.github.com>
2021-12-13 15:24:15 +0100

    Create 667.js

---
+++ js/html test/667.js
@@ -0,0 +1,26 @@
+/* 1st TEST
+alert("ekip");
+
+freeze = true;
+
+if(freeze){
+    alert("Prof Chen est présent");
+}
+*/
+
+/* 2nd TEST
+heure_arrivee = parseInt(prompt("Heure d'arrivée?"));
+//heure_arrivee = parseFloat(prompt("Heure d'arrivée?"));
+est_en_retard = heure_arrivee > 9;
+//alert(est_en_retard);
+if(est_en_retard){
+    alert("vous êtes en retard");
+}
+*/
+
+/* 3rd TEST
+if(heure_arrivee > 9){
+    alert("vous êtes en retard");
+}
+*/
+
