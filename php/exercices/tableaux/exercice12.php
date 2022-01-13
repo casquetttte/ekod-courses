d439ec83e375b0c7aa2f27a9b41791da89f4cac0
Anthony Biard <39205962+casquetttte@users.noreply.github.com>
2022-01-13 14:18:07 +0100

    Add files via upload

---
+++ php/exercices/tableaux/exercice12.php
@@ -0,0 +1,33 @@
+<!doctype html>
+<html lang="fr">
+<head>
+    <!-- CodeColliders 2020 - Exercice PHP -->
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Tableaux PHP - Exercice 12</title>
+</head>
+<body>
+<h1>Exercices Tableaux PHP</h1>
+<?php
+/**
+ * Cours-Exercices sur les tableaux PHP
+ */
+
+$tableDe = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+$nombres = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+
+/**
+ * Exercice 12:
+ * à l'aide de boucles foreach, parcourir le tableau $nombres pour afficher la table de multiplication de chacune des
+ * valeurs contenues dans le tableau $tableDe
+ * Chaque ligne du résultat généré doit ressembler à l'extrait suivant : "6 * 2 = 12"
+ */
+echo '<h2>Exercice 12</h2>';
+
+
+
+?>
+</body>
+</html>
